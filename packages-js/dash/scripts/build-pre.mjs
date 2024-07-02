c8db313bb3cd793d4c5c590882780a80805f1cef
2.30am <111672334+am230@users.noreply.github.com>
2024-07-02 13:08:22 +0900

    Update build-pre.mjs

--- packages-js/dash/scripts/build-pre.mjs
+++ packages-js/dash/scripts/build-pre.mjs
@@ -38,19 +38,8 @@ async function generateLicense() {
     );
 }
 
-async function generateVersion() {
-    const rootWorkspace = path.resolve(process.cwd(), '..', '..');
-    const lerna = JSON.parse(await fs.readFile(path.join(rootWorkspace, 'lerna.json'), 'utf8'));
-    const tauriConfigFile = path.join('src-tauri', 'tauri.conf.json');
-    const tauriConfig = JSON.parse(await fs.readFile(tauriConfigFile, 'utf8'));
-    tauriConfig.package.version = lerna.version;
-    await fs.writeFile(tauriConfigFile, JSON.stringify(tauriConfig, null, 4));
-    const versionFile = path.join('src', 'lib', 'version.json');
-    await fs.writeFile(versionFile, JSON.stringify({ version: lerna.version }));
-}
-
 async function main() {
-    const tasks = [generateLicense(), generateVersion()];
+    const tasks = [generateLicense()];
     await Promise.all(tasks);
 }
 
