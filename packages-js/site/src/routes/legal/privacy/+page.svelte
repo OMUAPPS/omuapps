bf9d4fddd97b397f01d514819a26b4a28517604b
2.30am <111672334+am230@users.noreply.github.com>
2024-07-07 05:51:52 +0900

    表記変更

--- packages-js/site/src/routes/legal/privacy/+page.svelte
+++ packages-js/site/src/routes/legal/privacy/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
     import Page from '$lib/components/Page.svelte';
+    import { FlexColWrapper, FlexRowWrapper } from '@omujs/ui';
+
+    import robotKilled from '$lib/images/robot-killed.svg';
 </script>
 
 <Page>
     <header slot="header">
-        <h1>
-            プライバシーポリシー
-            <i class="ti ti-file" />
-        </h1>
-        <small>使っていただける方へのお約束</small>
+        <FlexRowWrapper between alignItems="center">
+            <FlexColWrapper gap>
+                <h1>
+                    プライバシーポリシー
+                    <i class="ti ti-file" />
+                </h1>
+                <small>使っていただける方へお約束</small>
+            </FlexColWrapper>
+            <img src={robotKilled} alt="robot-reading" />
+        </FlexRowWrapper>
     </header>
     <main slot="content">
         <p>現在、プライバシーポリシーを作成中です。</p>
@@ -16,6 +24,11 @@
 </Page>
 
 <style lang="scss">
+    img {
+        width: 50%;
+        max-width: 16rem;
+    }
+
     h1 {
         font-size: 2rem;
         font-weight: 600;
--- packages-js/site/src/routes/legal/terms/+page.svelte
+++ packages-js/site/src/routes/legal/terms/+page.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
     import Page from '$lib/components/Page.svelte';
+    import { FlexColWrapper, FlexRowWrapper } from '@omujs/ui';
+
+    import robotReading from '$lib/images/robot-reading.svg';
 </script>
 
 <Page>
     <header slot="header">
-        <h1>
-            利用規約
-            <i class="ti ti-file" />
-        </h1>
-        <small> 使っていただくにあたって </small>
+        <FlexRowWrapper between alignItems="center">
+            <FlexColWrapper gap>
+                <h1>
+                    利用規約
+                    <i class="ti ti-file" />
+                </h1>
+                <small>使っていただくにあたって</small>
+            </FlexColWrapper>
+            <img src={robotReading} alt="robot-reading" />
+        </FlexRowWrapper>
     </header>
     <main slot="content">
         <p>現在、利用規約を作成中です。</p>
@@ -16,6 +24,11 @@
 </Page>
 
 <style lang="scss">
+    img {
+        width: 50%;
+        max-width: 16rem;
+    }
+
     h1 {
         font-size: 2rem;
         font-weight: 600;
