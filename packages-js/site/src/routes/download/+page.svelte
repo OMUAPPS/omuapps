f66f930ccd3f854ba8f59d7d6fc2953794b1baa6
2.30am <111672334+am230@users.noreply.github.com>
2024-07-13 23:07:59 +0900

    feat(site): ダウンロードページから規約・プライバシーポリシーに誘導するリンクを追加

--- packages-js/site/src/routes/download/+page.svelte
+++ packages-js/site/src/routes/download/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import Page from '$lib/components/Page.svelte';
-    import { Tooltip } from '@omujs/ui';
+    import { FlexColWrapper, FlexRowWrapper, Tooltip } from '@omujs/ui';
     import { BROWSER } from 'esm-env';
     import { getPlatform, type Platform, type VersionManifest } from './download.js';
 
@@ -42,18 +42,62 @@
     <main slot="content">
         <p>
             {#if version}
-                {@const date = new Date(data.manifest.pub_date)}
-                <a href={version?.url} class="download" on:click={() => (downloading = true)}>
-                    <Tooltip>
-                        {version.platform} 用のインストーラーをダウンロードします
-                    </Tooltip>
-                    {#if downloading}
-                        ダウンロード中...
-                    {:else}
-                        ダウンロード
-                    {/if}
-                    <i class="ti ti-download" />
+                <a href="/legal/terms" class="legal-link">
+                    <small>使っていただくにあたって</small>
+                    <p>
+                        利用規約
+                        <i class="ti ti-external-link" />
+                    </p>
                 </a>
+                <a href="/legal/privacy" class="legal-link">
+                    <small>使っていただける方へお約束</small>
+                    <p>
+                        プライバシーポリシー
+                        <i class="ti ti-external-link" />
+                    </p>
+                </a>
+                <p class="legal">
+                    OMUAPPSをダウンロードすることで、
+                    <br />
+                    利用規約とプライバシーポリシーに同意したものとみなされます。
+                </p>
+                <br />
+                {@const date = new Date(data.manifest.pub_date)}
+                <FlexRowWrapper>
+                    <a href={version?.url} class="download" on:click={() => (downloading = true)}>
+                        <Tooltip>
+                            {version.platform} 用のインストーラーをダウンロードします
+                        </Tooltip>
+                        {#if downloading}
+                            ダウンロード中...
+                        {:else}
+                            ダウンロード
+                        {/if}
+                        <i class="ti ti-download" />
+                    </a>
+                    <button on:click={() => (showExtra = !showExtra)} class="extra">
+                        <small>
+                            別のインストール方法
+                            <i class="ti ti-chevron-{showExtra ? 'up' : 'down'}" />
+                        </small>
+                    </button>
+                </FlexRowWrapper>
+                {#if showExtra}
+                    <ul>
+                        {#each Object.entries(data.manifest.platforms) as [key, platform] (key)}
+                            <li>
+                                <a
+                                    href={platform.url}
+                                    class="download"
+                                    on:click={() => (downloading = true)}
+                                >
+                                    {key}
+                                    <i class="ti ti-download" />
+                                </a>
+                            </li>
+                        {/each}
+                    </ul>
+                {/if}
                 <div class="version-info">
                     <p class="version">
                         {data.manifest.version}
@@ -68,28 +112,6 @@
                 <small> お使いのプラットフォームはサポートされていませんでした… </small>
             {/if}
         </p>
-        <button on:click={() => (showExtra = !showExtra)}>
-            <small>
-                別のインストール方法をお探しですか？
-                <i class="ti ti-chevron-{showExtra ? 'up' : 'down'}" />
-            </small>
-        </button>
-        {#if showExtra}
-            <ul>
-                {#each Object.entries(data.manifest.platforms) as [key, platform] (key)}
-                    <li>
-                        <a
-                            href={platform.url}
-                            class="download"
-                            on:click={() => (downloading = true)}
-                        >
-                            {key}
-                            <i class="ti ti-download" />
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        {/if}
     </main>
 </Page>
 
@@ -104,57 +126,85 @@
     .version-info {
         display: flex;
         gap: 1rem;
-        margin-top: 1rem;
         font-size: 0.9rem;
-
-        p {
-            color: var(--color-1);
-        }
+        color: var(--color-1);
 
         .version {
             font-weight: 600;
         }
     }
 
-    p {
-        text-wrap: nowrap;
+    .legal-link {
+        display: inline-flex;
+        flex-direction: column;
+        gap: 0.2rem;
+        padding: 0.4rem 0;
+        font-size: 1rem;
+        background: var(--color-bg-2);
+        margin-bottom: 0rem;
+        margin-right: 2rem;
+        margin-bottom: 2px;
 
-        a {
-            display: inline-flex;
-            gap: 0.2rem;
-            align-items: center;
-            padding: 0.6rem;
-            margin-right: 1rem;
-            margin-bottom: 1rem;
-            font-size: 1rem;
-            background: var(--color-bg-2);
-
-            &:hover {
-                background: var(--color-bg-3);
-            }
+        > small {
+            font-size: 0.6rem;
         }
 
-        .download {
-            color: var(--color-bg-2);
-            background: var(--color-1);
-
-            &:hover {
-                color: var(--color-1);
-                text-decoration: none;
-                background: var(--color-bg-2);
-                outline: 1px solid var(--color-1);
-            }
+        &:hover {
+            text-decoration: none;
+            border-bottom: 2px solid var(--color-1);
+            margin-bottom: 0px;
         }
     }
 
-    button {
-        display: flex;
+    .legal {
+        font-size: 0.8rem;
+        background: var(--color-bg-1);
+        color: var(--color-1);
+        margin-top: 1rem;
+        margin-bottom: 1.5rem;
+        padding: 0.5rem;
+        width: fit-content;
+    }
+
+    .download {
+        display: inline-flex;
+        gap: 0.2rem;
+        align-items: center;
+        padding: 0.6rem;
+        margin-right: 1rem;
+        margin-bottom: 1rem;
+        font-size: 1rem;
+        color: var(--color-bg-2);
+        background: var(--color-1);
+
+        &:hover {
+            color: var(--color-1);
+            text-decoration: none;
+            background: var(--color-bg-2);
+            outline: 1px solid var(--color-1);
+        }
+    }
+
+    .extra {
+        display: inline-flex;
+        gap: 0.2rem;
+        align-items: center;
+        padding: 0.6rem;
+        margin-right: 1rem;
+        margin-bottom: 1rem;
         font-size: 1rem;
         color: var(--color-1);
-        cursor: pointer;
-        background: none;
+        background: var(--color-bg-1);
+        outline: 1px solid var(--color-1);
+        outline-offset: -1px;
         border: none;
-        outline: none;
+
+        &:hover {
+            color: var(--color-1);
+            text-decoration: none;
+            background: var(--color-bg-2);
+            outline: 1px solid var(--color-1);
+        }
     }
 
     @container (max-width: 480px) {
