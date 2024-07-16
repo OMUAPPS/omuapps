e134c139459f8882ec3c556bf8a83186a56dbe69
2.30am <111672334+am230@users.noreply.github.com>
2024-07-17 01:56:15 +0900

    ページ機能

---
+++ packages-js/dash/src/lib/main/AppEntry.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+    import { omu } from '$lib/client.js';
+    import type { App } from '@omujs/omu';
+    import { Tooltip } from '@omujs/ui';
+    import { page } from './stores.js';
+
+    export let entry: App;
+
+    const metadata = entry.metadata;
+    const name = metadata?.name ? omu.i18n.translate(metadata?.name) : entry.id.key();
+    const description = metadata?.description
+        ? omu.i18n.translate(metadata?.description)
+        : 'No description';
+    const icon = metadata?.icon ? omu.i18n.translate(metadata?.icon) : 'No icon';
+    const image = metadata?.image ? omu.i18n.translate(metadata?.image) : null;
+
+    function handleClick() {
+        $page = entry.url || null;
+    }
+</script>
+
+<button class="app" on:click={handleClick}>
+    <Tooltip>
+        <div class="tooltip">
+            <h3>{name}</h3>
+            <small>{description}</small>
+        </div>
+    </Tooltip>
+    {#if image}
+        <img src={image} alt="" class="image" />
+    {/if}
+    <div class="info">
+        <span class="icon">
+            {#if icon.startsWith('ti-')}
+                <i class="ti {icon}"></i>
+            {:else}
+                <img src={icon} alt={name} />
+            {/if}
+        </span>
+        <p class="name">
+            {name}
+        </p>
+        <button class="open">
+            <i class="ti ti-chevron-right" />
+        </button>
+    </div>
+</button>
+
+<style lang="scss">
+    .app {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        background: var(--color-bg-2);
+        border: none;
+        border-bottom: 1px solid var(--color-outline);
+        width: 100%;
+        height: 4rem;
+
+        &:hover {
+            background: var(--color-bg-1);
+            outline: 1px solid var(--color-1);
+            outline-offset: -3px;
+            transition: background 0.0621s;
+
+            > .info {
+                margin: 0.1rem;
+                transition: margin 0.0621s;
+
+                > .open {
+                    display: block;
+                    margin-right: 0rem;
+                    transition: margin 0.0621s;
+                }
+            }
+        }
+
+        .tooltip {
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            text-wrap: wrap;
+            text-align: start;
+            padding: 0.1rem;
+
+            > h3 {
+                font-size: 0.9rem;
+                font-weight: 600;
+                color: var(--color-text-1);
+                width: 100%;
+            }
+
+            > small {
+                font-size: 0.621rem;
+                color: var(--color-text-2);
+            }
+        }
+
+        > .image {
+            position: absolute;
+            inset: 0;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            opacity: 0.5;
+        }
+
+        > .info {
+            position: absolute;
+            inset: 0;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 1rem;
+            width: 100%;
+            color: var(--color-1);
+
+            > .icon {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 2rem;
+                height: 2rem;
+                margin-right: 0.25rem;
+                background: var(--color-bg-3);
+
+                > img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
+                }
+
+                > .ti {
+                    font-size: 1.25rem;
+                }
+            }
+
+            > .name {
+                font-size: 0.8rem;
+                font-weight: 600;
+                color: var(--color-text-1);
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+
+            > .open {
+                display: none;
+                margin-left: auto;
+                margin-right: 0.1rem;
+                min-width: 2rem;
+                height: 2rem;
+                color: var(--color-1);
+                background: none;
+                border: none;
+            }
+        }
+    }
+</style>
--- packages-js/dash/src/lib/main/MainWindow.svelte
+++ packages-js/dash/src/lib/main/MainWindow.svelte
@@ -1,153 +1,166 @@
 <script lang="ts">
-    import { writable } from 'svelte/store';
-
-    import ButtonOpenSettings from './ButtonOpenSettings.svelte';
-    import PageDev from './page/dev/PageDev.svelte';
-    import PageHome from './page/home/PageHome.svelte';
-    import { pages } from './page/page.js';
-    import PageChannels from './page/PageChannels.svelte';
-    import PageMessages from './page/PageMessages.svelte';
-    import { currentPage, devMode } from './settings.js';
-
-    import { t } from '$lib/i18n/i18n-context.js';
-    import { style } from '$lib/utils/class-helper.js';
-    import { FlexColWrapper, FlexRowWrapper, Tooltip } from '@omujs/ui';
-
-    pages.set(new Map());
-    $pages.set('main', {
-        name: 'main',
-        component: PageHome,
-        props: {},
-    });
-    $pages.set('message', {
-        name: 'message',
-        component: PageMessages,
-        props: {},
-    });
-    $pages.set('channel', {
-        name: 'channel',
-        component: PageChannels,
-        props: {},
-    });
-
-    $: if ($devMode) {
-        $pages.set('dev', {
-            name: 'dev',
-            component: PageDev,
-            props: {},
-        });
-    } else {
-        $pages.delete('dev');
-    }
-
-    let cachedPages = writable(new Set<string>([$currentPage]));
-
-    $: {
-        cachedPages.update((set) => {
-            set.add($currentPage);
-            return set;
-        });
-    }
-
-    function handleKeydown(event: KeyboardEvent) {
-        // ctrl + [1234567890] to switch pages
-        const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-        if (event.ctrlKey && numbers.includes(event.key)) {
-            const index = numbers.indexOf(event.key);
-            if (index >= $pages.size) return;
-            $currentPage = [...$pages.keys()][index];
-            event.preventDefault();
-            return;
-        }
-    }
+    import { dashboard } from '$lib/client.js';
+    import { TableList, Tooltip } from '@omujs/ui';
+    import AppEntry from './AppEntry.svelte';
+    import { page } from './stores.js';
 </script>
 
-<svelte:window on:keydown={handleKeydown} />
-
-<FlexRowWrapper heightFull>
-    <div class="tab-container">
-        <FlexColWrapper>
-            {#each $pages.entries() as [key, page] (key)}
-                <button
-                    class="page"
-                    on:click={() => {
-                        $currentPage = page.name;
-                    }}
-                    class:active={$currentPage === page.name}
-                >
-                    <Tooltip>{$t(`pages.${page.name}.name`)}</Tooltip>
-                    <i class={$t(`pages.${page.name}.icon`)} />
+<main>
+    <div class="tabs">
+        <button class="tab" on:click={() => ($page = 'http://localhost:5173/app/?hide=1')}>
+            <i class="ti ti-search" />
+            <span>アプリを探す</span>
+            <i class="open ti ti-chevron-right" />
+        </button>
+        <button class="tab">
+            <i class="ti ti-message" />
+            <span>配信をつなげる</span>
+            <i class="open ti ti-chevron-right" />
+        </button>
+        <button class="tab">
+            <i class="ti ti-settings" />
+            <span>設定</span>
+            <i class="open ti ti-chevron-right" />
+        </button>
+        <div class="tab-group">
+            <span>アプリ</span>
+            <div class="buttons">
+                <button>
+                    <Tooltip>
+                        <div class="tooltip">
+                            <h3>アプリを追加</h3>
+                            <small>新しいアプリを追加します</small>
+                        </div>
+                    </Tooltip>
+                    <i class="ti ti-settings" />
                 </button>
-            {/each}
-        </FlexColWrapper>
-        <FlexColWrapper>
-            <ButtonOpenSettings />
-        </FlexColWrapper>
-    </div>
-    {#each $pages.entries() as [key, page] (key)}
-        <div
-            style={style({ display: $currentPage === page.name ? '' : 'none' })}
-            class="page-container"
-        >
-            {#if $cachedPages.has(page.name)}
-                <svelte:component this={page.component} {...page.props} />
-            {/if}
+                <button>
+                    <Tooltip>
+                        <div class="tooltip">
+                            <h3>アプリを並び替え</h3>
+                            <small>アプリの並び順を変更します</small>
+                        </div>
+                    </Tooltip>
+                    <i class="ti ti-chevron-down" />
+                </button>
+            </div>
         </div>
-    {/each}
-</FlexRowWrapper>
+        <div class="list">
+            <TableList table={dashboard.apps} component={AppEntry} />
+        </div>
+    </div>
+    <div class="page">
+        {#if $page}
+            <iframe src={$page} title="" frameborder="0" allow="camera; microphone"></iframe>
+        {/if}
+    </div>
+</main>
 
 <style lang="scss">
-    .wrapper {
+    main {
+        display: flex;
+        flex-direction: row;
+        height: 100%;
+        background: var(--color-bg-1);
+        font-weight: 600;
+    }
+
+    .tabs {
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+        background: var(--color-bg-2);
+        border-right: 1px solid var(--color-outline);
+        padding: 1rem 0.5rem;
+    }
+
+    .tab {
         display: flex;
         flex-direction: row;
         align-items: center;
-        height: 100%;
+        padding: 0 1rem;
+        border: none;
+        background: var(--color-bg-2);
+        color: var(--color-1);
+        width: 100%;
+        font-size: 0.9rem;
+        font-weight: 600;
+        height: 3rem;
+
+        > i {
+            font-size: 1rem;
+            margin-right: 1rem;
+        }
+
+        > .open {
+            margin-left: auto;
+            display: none;
+        }
+
+        &:hover {
+            background: var(--color-bg-1);
+            outline: 1px solid var(--color-1);
+            outline-offset: -3px;
+            transition: background 0.0621s;
+
+            > .open {
+                display: block;
+                margin-right: 0rem;
+                transition: margin 0.0621s;
+            }
+        }
     }
 
-    .tab-container {
-        z-index: 1;
+    .tab-group {
         display: flex;
-        flex-direction: column;
-        gap: 10px;
-        align-items: center;
-        justify-content: space-between;
-        width: 40px;
-        height: 100%;
-        padding-top: 45px;
-        padding-bottom: 40px;
+        flex-direction: row;
+        align-items: baseline;
+        padding: 0.5rem 0rem;
         background: var(--color-bg-2);
-        outline: 1px solid var(--color-outline);
+        color: var(--color-1);
+        border-bottom: 1px solid var(--color-outline);
+        width: 100%;
+        font-size: 0.85rem;
+        padding-bottom: 0.75rem;
+        margin-top: 1rem;
+        padding-left: 1rem;
+
+        > i {
+            margin-left: 0.25rem;
+        }
+
+        > .buttons {
+            margin-left: auto;
+
+            > button {
+                background: none;
+                border: none;
+                color: var(--color-1);
+                font-size: 1rem;
+                font-weight: 600;
+                width: 2rem;
+                height: 2rem;
+
+                &:hover {
+                    background: var(--color-1);
+                    color: var(--color-bg-1);
+                    border-radius: 4px;
+                }
+            }
+        }
     }
 
     .page {
-        width: 40px;
-        height: 40px;
-        font-size: 16px;
-        color: color-mix(in srgb, var(--color-1) 75%, var(--color-bg-2) 0%);
-        background: none;
-        background: var(--color);
-        border: none;
-        outline: none;
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: var(--color-bg-1);
 
-        &:hover {
-            color: var(--color-1);
-            background: var(--color-bg-2);
-            outline: 1px solid var(--color-1);
-            outline-offset: -3px;
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
         }
-
-        &.active {
-            z-index: 2;
-            color: var(--color-1);
-            background: color-mix(in srgb, var(--color-1) 12%, transparent 0%);
-            border-left: 4px solid var(--color-1);
-            outline: 2px solid var(--color-bg-2);
-            outline-offset: -2px;
-        }
-    }
-
-    .page-container {
-        width: calc(100% - 40px);
-        height: 100%;
     }
 </style>
