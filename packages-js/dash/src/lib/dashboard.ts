ca2766d8ad5783d6d6775a428ff9b3c63a84a929
2.30am <111672334+am230@users.noreply.github.com>
2024-07-17 16:28:53 +0900

    fix(dash): アプリを別ウィンドウで開かずにページとして開くように

--- packages-js/dash/src/lib/dashboard.ts
+++ packages-js/dash/src/lib/dashboard.ts
@@ -1,3 +1,5 @@
+import PermissionRequestScreen from '$lib/common/screen/PermissionRequestScreen.svelte';
+import PluginRequestScreen from '$lib/common/screen/PluginRequestScreen.svelte';
 import { currentPage } from '$lib/main/settings.js';
 import { tauriWindow } from '$lib/utils/tauri.js';
 import { App, Omu } from '@omujs/omu';
@@ -6,13 +8,10 @@ import type {
     PermissionRequestPacket,
     PluginRequestPacket,
 } from '@omujs/omu/extension/dashboard/packets.js';
+import type { Table } from '@omujs/omu/extension/table/table.js';
 import { Identifier } from '@omujs/omu/identifier.js';
 import type { Locale } from '@omujs/omu/localization/locale.js';
 import { screenContext } from './common/screen/screen.js';
-import { omu } from './client.js';
-import PermissionRequestScreen from '$lib/common/screen/PermissionRequestScreen.svelte';
-import PluginRequestScreen from '$lib/common/screen/PluginRequestScreen.svelte';
-import type { Table } from '@omujs/omu/extension/table/table.js';
 
 export const IDENTIFIER = Identifier.fromKey('com.omuapps:dashboard');
 
@@ -52,15 +51,6 @@ export class Dashboard implements DashboardHandler {
     }
 
     async handleOpenApp(app: App): Promise<void> {
-        const windowLabel =
-            app.id.namespace.replace(/\./g, '-') + '-' + btoa(app.id.key()).replace(/=/g, '');
-        const windowTitle = omu.i18n.translate(app.metadata?.name ?? app.id.key());
-        const window = new tauriWindow.WebviewWindow(windowLabel, {
-            url: app.url,
-            title: windowTitle,
-            fileDropEnabled: true,
-            transparent: true,
-        });
-        window.setFocus();
+        currentPage.set(`app-${app.id.key()}`);
     }
 }
