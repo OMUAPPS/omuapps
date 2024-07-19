7c44b4975f667ffa9196f664546bd4a8f81dcda9
2.30am <111672334+am230@users.noreply.github.com>
2024-07-20 04:22:25 +0900

    fix(dash): iframeを使うページを開いてても下と右側でウィンドウサイズを変えれるように

--- packages-js/dash/src/lib/main/pages/AppPage.svelte
+++ packages-js/dash/src/lib/main/pages/AppPage.svelte
@@ -6,12 +6,42 @@
     };
 </script>
 
-<iframe src={props.app.url} title="" frameborder="0" allow="camera; microphone"></iframe>
+<div class="container">
+    <iframe src={props.app.url} title="" frameborder="0" allow="camera; microphone"></iframe>
+    <div class="window-resize bottom" />
+    <div class="window-resize right" />
+</div>
 
 <style lang="scss">
+    .container {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+
     iframe {
+        position: absolute;
+        inset: 0;
         width: 100%;
         height: 100%;
         border: none;
+    }
+
+    .window-resize {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+
+        &.bottom {
+            left: 0;
+            height: 5px;
+            cursor: ns-resize;
+        }
+
+        &.right {
+            top: 0;
+            width: 5px;
+            cursor: ew-resize;
+        }
     }
 </style>
