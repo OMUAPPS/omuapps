b9946f201a0eb9be53801ef8f85a28a3a6a15a78
2.30am <111672334+am230@users.noreply.github.com>
2024-07-19 17:36:21 +0900

    feat(aoikuru-fries): 使い方に関するヒントを追加

--- packages-js/site/src/routes/app/aoikuru-fries/+page.svelte
+++ packages-js/site/src/routes/app/aoikuru-fries/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import AppPage from '$lib/components/AppPage.svelte';
     import { Omu } from '@omujs/omu';
-    import { setClient } from '@omujs/ui';
+    import { setClient, Tooltip } from '@omujs/ui';
     import { BROWSER } from 'esm-env';
     import { APP } from './app.js';
     import { FriesApp } from './fries-app.js';
+    import board from './img/board.png';
+    import hint from './img/hint.png';
 
     const omu = new Omu(APP);
     setClient(omu);
@@ -18,30 +20,133 @@
     if (BROWSER) {
         omu.start();
     }
+
+    $: stateText = {
+        idle: '待機中',
+        idle_start: '待機開始',
+        throwing: '投げ中',
+        thrown: '投げ終わり',
+        catching: 'キャッチ中',
+        eating: '食べ中',
+        throw_many: 'たくさん投げる',
+        throw_many_hit: 'たくさん投げる当たり',
+        throw_start: '投げ開始',
+    }[$state.type];
 </script>
 
 <AppPage>
     <main>
-        <h1>aoikuru-fries</h1>
-        <h3>test</h3>
-        <button on:click={() => friesApp.test()}>Test</button>
-        <h3>config</h3>
-        <input type="text" bind:value={$config.hint} />
-        <h3>text</h3>
-        <textarea bind:value={$config.text} />
-        3行以上は崩れる覚悟で
-        <h3>state</h3>
-        {JSON.stringify($state)}
+        <section>
+            <div>
+                <h3>試しに投げてみる</h3>
+                <button on:click={() => friesApp.test()}>
+                    投げる
+                    <i class="ti ti-arrow-up-right" />
+                </button>
+            </div>
+        </section>
+        <section>
+            <div>
+                <Tooltip>
+                    <img src={hint} alt="" />
+                </Tooltip>
+                <h3>ヒント</h3>
+                <textarea bind:value={$config.hint} class="hint" />
+            </div>
+            <img src={hint} alt="" />
+        </section>
+        <section>
+            <div>
+                <h3>看板</h3>
+                <textarea bind:value={$config.text} class="text" />
+            </div>
+            <img src={board} alt="" />
+        </section>
+        <section>
+            <div>
+                <h3>状態</h3>
+                <code>
+                    {stateText}
+                </code>
+                <span class="state">
+                    {JSON.stringify($state)}
+                </span>
+            </div>
+        </section>
     </main>
 </AppPage>
 
 <style lang="scss">
     main {
-        padding: 20px;
+        padding: 2rem;
+    }
+
+    section {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        width: 50rem;
+        max-width: 100%;
+
+        img {
+            width: 12rem;
+            object-fit: contain;
+        }
+    }
+
+    h3 {
+        margin-top: 1rem;
+        color: var(--color-1);
     }
 
     textarea {
-        width: 100%;
-        height: 200px;
+        margin-top: 0.5rem;
+        width: 20rem;
+        height: 5rem;
+        background: var(--color-bg-2);
+        border: 1px solid var(--color-outline);
+        padding: 0.5rem;
+
+        &:focus,
+        &:hover {
+            border-color: var(--color-1);
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    button {
+        margin-top: 0.5rem;
+        padding: 0.5rem 1rem;
+        background: var(--color-1);
+        color: var(--color-bg-1);
+        font-size: 0.9rem;
+        font-weight: 500;
+        border: none;
+        border-radius: 2.5px;
+        cursor: pointer;
+
+        &:hover {
+            background: var(--color-1);
+        }
+    }
+
+    code {
+        display: block;
+        width: fit-content;
+        margin-top: 0.5rem;
+        padding: 0.5rem 0.75rem;
+        color: var(--color-1);
+        font-size: 0.9rem;
+        border-left: 2px solid var(--color-1);
+    }
+
+    .state {
+        font-weight: 600;
+        font-size: 0.8rem;
+        color: #444;
     }
 </style>
