604434a53c4ea0d50491fe2fa3dabbacb38218b0
2.30am <111672334+am230@users.noreply.github.com>
2024-08-08 23:39:07 +0900

    fix(dash): ILocale型をtypeの代わりにRecordを使うようにする

--- packages-js/dash/src/lib/i18n/i18n.ts
+++ packages-js/dash/src/lib/i18n/i18n.ts
@@ -1,13 +1,14 @@
-import { loadI18n } from "@omujs/i18n";
-import type { I18n } from "@omujs/i18n";
-import LOCALELIST from "./locales/_list.json" with { type: "json" };
+import type { I18n } from '@omujs/i18n';
+import { loadI18n } from '@omujs/i18n';
+import LOCALELIST from './locales/_list.json' with { type: 'json' };
 
-type ILocale = {
-    [key: string]: {
+type ILocale = Record<
+    string,
+    {
         name: string;
         load: () => Promise<I18n>;
     }
-};
+>;
 
 export const LOCALES: ILocale = {};
 
@@ -18,4 +19,4 @@ LOCALELIST.map((locale) => {
     };
 });
 
-export const DEFAULT_LOCALE = "ja-JP";
+export const DEFAULT_LOCALE = 'ja-JP';
