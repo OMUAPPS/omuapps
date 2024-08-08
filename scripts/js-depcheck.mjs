65f56c4de8476268ea72a2dd9740e936768e6279
貴雪❆ <71214511+f1w3@users.noreply.github.com>
2024-08-09 03:21:25 +0900

    refactor: package.jsonの書式を統一する

---
+++ scripts/js-depcheck.mjs
@@ -0,0 +1,18 @@
+import { execa } from "execa"
+
+const paths = [
+    "packages-js/chat",
+    "packages-js/dash",
+    "packages-js/i18n",
+    "packages-js/omu",
+    "packages-js/plugin-obs",
+    "packages-js/shared",
+    "packages-js/site",
+    "packages-js/ui"
+]
+
+for await (const path of paths) {
+    console.log(`\n=== ${path} ===`)
+    await execa('pnpm depcheck', [`${path}`], { stderr: process.stderr, stdout: process.stdout })
+        .catch(() => { })
+}
