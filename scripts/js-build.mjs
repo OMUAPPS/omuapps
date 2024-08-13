447d46a472f20e69f8040634a617fee5d6a21976
2.30am <111672334+am230@users.noreply.github.com>
2024-08-13 23:43:26 +0900

    chore: srcが変えられてなければビルドをスキップする (#68)
    * change: ビルド時にキャッシュがある場合ビルドをスキップするようにする
    
    * refactor
    
    ---------
    
    Co-authored-by: 貴雪❆ <71214511+f1w3@users.noreply.github.com>

--- scripts/js-build.mjs
+++ scripts/js-build.mjs
@@ -1,22 +1,63 @@
+import { createHash } from 'crypto';
 import { execa } from 'execa';
+import { readdirSync, readFileSync } from 'fs';
+import { readdir, stat, writeFile } from 'fs/promises';
+import Path from 'path';
 
-const option = { stderr: process.stderr, stdout: process.stdout }
+const BUILD_OPTION = { stderr: process.stderr, stdout: process.stdout }
+const PACKAGES_DIR = Path.join(process.cwd(), 'packages-js');
+
+const packages = new Map([...readdirSync(PACKAGES_DIR)].map((name) => {
+    const path = Path.join(PACKAGES_DIR, name);
+    const pkg = JSON.parse(readFileSync(Path.join(path, 'package.json'), 'utf-8'));
+    return [pkg.name, { path, pkg }];
+}));
+
+async function computeDirHash(dir) {
+    const hash = createHash('md5');
+    const paths = [...await readdir(dir)].map((name) => Path.join(dir, name));
+    while (paths.length) {
+        const path = paths.pop();
+        const fileStat = await stat(path);
+        if (fileStat.isDirectory()) {
+            const filePaths = [...await readdir(path)].map((name) => Path.join(path, name));
+            paths.push(...filePaths);
+        }
+        hash.update(fileStat.mtime.toString());
+    }
+    return hash.digest('hex');
+}
+
+async function buildPackage(target) {
+    const pkg = packages.get(target);
+    if (!pkg) {
+        throw new Error(`Package ${target} not found. ${[...packages.keys()]}`);
+    }
+    const hash = await computeDirHash(Path.join(pkg.path, 'src'));
+    const lastHash = pkg.pkg.srcHash;
+    if (hash === lastHash) {
+        return;
+    }
+    await execa('pnpm', ['--filter', pkg.pkg.name, 'build'], BUILD_OPTION);
+    const newHash = await computeDirHash(Path.join(pkg.path, 'src'));
+    pkg.pkg.srcHash = newHash;
+    await writeFile(Path.join(pkg.path, 'package.json'), JSON.stringify(pkg.pkg, null, 4));
+}
 
 await Promise.all([
-    execa('pnpm', ['--filter', 'ui', 'build'], option),
-    execa('pnpm', ['--filter', 'i18n', 'build'], option),
-    execa('pnpm', ['--filter', 'omu', 'build'], option),
+    buildPackage('@omujs/ui'),
+    buildPackage('@omujs/i18n'),
+    buildPackage('@omujs/omu'),
 ]);
 
-await execa('pnpm', ['--filter', 'chat', 'build'], option);
-
 await Promise.all([
-    execa('pnpm', ['--filter', 'obs', 'build'], option),
+    buildPackage('@omujs/chat'),
+    buildPackage('@omujs/obs'),
 ]);
 
 if (process.argv.includes('--build')) {
     const targets = process.argv[process.argv.indexOf('--build') + 1];
     Promise.all(targets.split(',').map((target) =>
-        execa('pnpm', ['--filter', target, 'build'], option)
+        execa('pnpm', ['--filter', target, 'build'], BUILD_OPTION)
     ))
 }
