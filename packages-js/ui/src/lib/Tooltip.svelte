62623c077a4d1d0c5efd965054018d68de519525
2.30am <111672334+am230@users.noreply.github.com>
2024-08-20 05:20:04 +0900

    refactor(ui): Tooltip.svelte

--- packages-js/ui/src/lib/Tooltip.svelte
+++ packages-js/ui/src/lib/Tooltip.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
-    import { BROWSER } from 'esm-env';
-    import { afterUpdate, onDestroy } from 'svelte';
-
     export let noBackground = false;
-
-    let element: HTMLElement;
+    export let show = false;
     let target: HTMLElement;
+
     let tooltip: HTMLElement;
-    let show = false;
     type Rect = { x: number; y: number; width: number; height: number };
     let tooltipRect: Rect = {
         x: 0,
@@ -22,9 +18,8 @@
         height: 0,
     };
     let tooltipPos: { x: number; y: number } = { x: 0, y: 0 };
-    let pointerPos: { x: number; y: number } = { x: 0, y: 0 };
     let offset: { x: number; y: number } = { x: 0, y: 0 };
-    let direction: 'top' | 'bottom' = 'bottom';
+    let direction: 'up' | 'down' = 'down';
 
     function showTooltip() {
         show = true;
@@ -35,19 +30,13 @@
         show = false;
     }
 
-    function clamp(value: number, min: number, max: number) {
-        return Math.min(Math.max(value, min), max);
-    }
-
     function getPositionRoot(element: HTMLElement): { x: number; y: number } {
-        const resetStyles = { 'container-type': ['inline-size'] };
         let currentElement: HTMLElement | null = element;
         while (currentElement) {
             const style = getComputedStyle(currentElement);
             if (
-                Object.entries(resetStyles).some(([key, values]) =>
-                    values.includes(style.getPropertyValue(key)),
-                )
+                style.getPropertyValue('container-type') === 'inline-size' ||
+                style.getPropertyValue('transform') !== 'none'
             ) {
                 const rect = currentElement.getBoundingClientRect();
                 return {
@@ -60,68 +49,85 @@
         return { x: 0, y: 0 };
     }
 
-    $: {
-        const padding = 5;
+    function clamp(value: number, min: number, max: number) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    function update(target: HTMLElement, tooltip: HTMLElement): void {
+        const margin = 10;
+        const arrowSize = 10;
         if (target && tooltip) {
             tooltipRect = tooltip.getBoundingClientRect();
-            direction =
-                targetRect.y + targetRect.height + tooltipRect.height + 10 > window.innerHeight
-                    ? 'top'
-                    : 'bottom';
+            const targetBounds = {
+                top: targetRect.y,
+                bottom: targetRect.y + targetRect.height,
+                centerX: targetRect.x + targetRect.width / 2,
+            };
+            const bounds = {
+                left: margin,
+                top: margin,
+                right: window.innerWidth - margin,
+                bottom: window.innerHeight - margin,
+            };
+            direction = targetBounds.bottom + tooltipRect.height > bounds.bottom ? 'up' : 'down';
             tooltipPos = {
                 x: clamp(
-                    targetRect.x + targetRect.width / 2 - tooltipRect.width / 2,
-                    padding,
-                    window.innerWidth - tooltipRect.width - padding,
+                    targetBounds.centerX - tooltipRect.width / 2,
+                    bounds.left,
+                    bounds.right - tooltipRect.width,
                 ),
                 y: clamp(
-                    direction === 'bottom'
-                        ? targetRect.y + targetRect.height + 10
-                        : targetRect.y - tooltipRect.height - 10,
-                    padding,
-                    window.innerHeight - tooltipRect.height - padding,
+                    direction === 'down'
+                        ? targetBounds.bottom + arrowSize
+                        : targetBounds.top - tooltipRect.height - arrowSize,
+                    bounds.top,
+                    bounds.bottom - tooltipRect.height,
                 ),
             };
             offset = getPositionRoot(target);
         }
     }
 
-    if (BROWSER) {
-        afterUpdate(() => {
-            if (!element.parentElement) {
-                throw new Error('TooltipInline must be a child of another element');
-            }
-            target = element.parentElement;
-            if (!target.addEventListener || !target.removeEventListener) {
-                throw new Error('target must support addEventListener and removeEventListener');
-            }
-            target.addEventListener('mouseenter', showTooltip);
-            target.addEventListener('mouseleave', hideTooltip);
-        });
+    $: {
+        update(target, tooltip);
+    }
 
-        onDestroy(() => {
-            target.removeEventListener('mouseenter', showTooltip);
-            target.removeEventListener('mouseleave', hideTooltip);
-        });
+    function attachParent(node: HTMLElement) {
+        if (!node.parentElement) {
+            throw new Error('TooltipInline must be a child of another node');
+        }
+        target = node.parentElement;
+        if (!target.addEventListener || !target.removeEventListener) {
+            throw new Error('target must support addEventListener and removeEventListener');
+        }
+        target.addEventListener('mouseenter', showTooltip);
+        target.addEventListener('mouseleave', hideTooltip);
+
+        return {
+            destroy() {
+                target.removeEventListener('mouseenter', showTooltip);
+                target.removeEventListener('mouseleave', hideTooltip);
+            },
+        };
     }
 </script>
 
-<span class="wrapper" bind:this={element}>
+<span class="wrapper" use:attachParent>
     {#if show}
         <div
             class="tooltip"
             class:background={!noBackground}
-            class:top={direction === 'top'}
+            class:top={direction === 'up'}
             style="left: {tooltipPos.x - offset.x}px; top: {tooltipPos.y - offset.y}px;"
             bind:this={tooltip}
         >
             <slot />
         </div>
         <div
-            class="pointer"
-            class:top={direction === 'top'}
+            class="arrow"
+            class:top={direction === 'up'}
             style="left: {targetRect.x + targetRect.width / 2 - offset.x}px; top: {(direction ===
-            'bottom'
+            'down'
                 ? targetRect.y + targetRect.height
                 : targetRect.y - 10) - offset.y}px;"
         />
@@ -145,18 +151,18 @@
         }
     }
 
-    .pointer {
+    .arrow {
         position: fixed;
         z-index: 20;
         pointer-events: none;
         content: '';
         user-select: none;
-        border: 5px solid transparent;
+        border: 6px solid transparent;
         border-bottom-color: #000;
         transform: translateX(-50%);
 
         &.top {
-            transform: translateX(-50%) rotate(180deg);
+            transform: translateX(-50%) scaleY(-1);
         }
     }
 
