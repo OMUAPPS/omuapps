6e2a8a0c75f8ea4474d5e46033249110767268dc
2.30am <111672334+am230@users.noreply.github.com>
2024-08-29 19:29:12 +0900

    chore: sveltejs/packageにパッチを適応して起動できるように

---
+++ scripts/js-patch.mjs
@@ -0,0 +1,36 @@
+import * as fs from 'fs/promises';
+import { glob } from 'glob';
+
+const original = `/** @param {string} path */
+export function rimraf(path) {
+    fs.rmSync(path, { force: true, recursive: true });
+}`
+
+const patch = `/** @param {string} path */
+export function rimraf(path) {
+    try {
+        fs.rmSync(path, { force: true, recursive: true });
+    } catch (/** @type {any} */ e) {
+    }
+}`
+
+const found = await glob('node_modules/.pnpm/@sveltejs+package@*/**/node_modules/@sveltejs/package/src/filesystem.js');
+if (!found.length) {
+    throw new Error('File not found');
+}
+
+for (const path of found) {
+    let content = await fs.readFile(path, 'utf-8');
+    console.log(content.split('\n').slice(13, 18).join('\n'));
+    const alreadyPatched = content.includes(patch);
+    if (alreadyPatched) {
+        console.log(`Already patched ${path}`);
+    } else {
+        if (!content.includes(original)) {
+            throw new Error('Original code not found');
+        } else {
+            content = content.replace(original, patch);
+            await fs.writeFile(path, content, 'utf-8');
+        }
+    }
+}
