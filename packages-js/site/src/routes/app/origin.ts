3dc01bcc36ae4d20d8252ae9fc67e56117d3ff2b
2.30am <111672334+am230@users.noreply.github.com>
2024-09-07 03:43:26 +0900

    Update origin.ts

--- packages-js/site/src/routes/app/origin.ts
+++ packages-js/site/src/routes/app/origin.ts
@@ -1,10 +1,9 @@
-import { Identifier } from '@omujs/omu';
 import { DEV } from 'esm-env';
 
-export const ORIGIN = DEV ? 'http://localhost:5173' : new URL(process.env.CF_PAGES_URL).origin;
+export const ORIGIN = DEV ? 'http://localhost:5173' : 'https://omuapps.com';
 export function getUrl(path: string): string {
     const url = new URL(path, ORIGIN);
     return url.href;
 }
 
-export const NAMESPACE = DEV ? 'com.omuapps' : Identifier.namespaceFromUrl(process.env.CF_PAGES_URL);
+export const NAMESPACE = 'com.omuapps';
