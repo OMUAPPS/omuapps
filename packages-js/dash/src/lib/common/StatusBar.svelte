7e2deb7ce364b0f29d55f20e1baf46cdf292f985
2.30am <111672334+am230@users.noreply.github.com>
2024-10-13 13:21:41 +0900

    Update StatusBar.svelte

--- packages-js/dash/src/lib/common/StatusBar.svelte
+++ packages-js/dash/src/lib/common/StatusBar.svelte
@@ -1,59 +1,42 @@
 <script lang="ts">
     import { onDestroy } from 'svelte';
-    import { writable } from 'svelte/store';
-
+    
     import { NetworkStatus } from '@omujs/omu/network/network.js';
-    import { FlexColWrapper, Tooltip } from '@omujs/ui';
     import { omu } from '../client.js';
 
-    const status = writable<NetworkStatus>(omu.network.status);
+    let status: NetworkStatus = omu.network.status;
 
-    const unlisten = omu.network.event.status.listen((newStatus) => status.set(newStatus));
+    const unlisten = omu.network.event.status.listen((newStatus) => {
+        status = newStatus;
+    });
     onDestroy(unlisten);
 </script>
 
-<p class={$status}>
-    {#if $status === NetworkStatus.CONNECTED}
+<p class={status}>
+    {#if status === NetworkStatus.CONNECTED}
         <i class="ti ti-bolt" />
-    {/if}
-    {#if $status === NetworkStatus.READY}
+        接続済み
+        <span>
+            {omu.network.address.host}:{omu.network.address.port}
+            <small> に接続済み </small>
+        </span>
+    {:else if status === NetworkStatus.READY}
         <i class="ti ti-check" />
-    {/if}
-    {#if $status === NetworkStatus.CONNECTING}
+    {:else if status === NetworkStatus.CONNECTING}
         <i class="ti ti-reload" />
-    {/if}
-    {#if $status === NetworkStatus.DISCONNECTED}
+        接続中
+        <span>
+            {omu.network.address.host}:{omu.network.address.port}
+            <small> に接続中… </small>
+        </span>
+    {:else if status === NetworkStatus.DISCONNECTED}
         <i class="ti ti-x" />
+        接続されていません
+        <span>
+            {omu.network.address.host}:{omu.network.address.port}
+            <small> に接続できませんでした </small>
+        </span>
     {/if}
-    <Tooltip>
-        {#if $status === NetworkStatus.CONNECTED}
-            <FlexColWrapper>
-                接続済み
-                <span>
-                    {omu.network.address.host}:{omu.network.address.port}
-                    <small> に接続済み </small>
-                </span>
-            </FlexColWrapper>
-        {/if}
-        {#if $status === 'connecting'}
-            <FlexColWrapper>
-                接続中
-                <span>
-                    {omu.network.address.host}:{omu.network.address.port}
-                    <small> に接続中… </small>
-                </span>
-            </FlexColWrapper>
-        {/if}
-        {#if $status === 'disconnected'}
-            <FlexColWrapper>
-                接続されていません
-                <span>
-                    {omu.network.address.host}:{omu.network.address.port}
-                    <small> に接続できませんでした </small>
-                </span>
-            </FlexColWrapper>
-        {/if}
-    </Tooltip>
 </p>
 
 <style>
