25e94fa01f33d40c0d643e5054d334608d53d015
2.30am <111672334+am230@users.noreply.github.com>
2024-10-19 02:58:03 +0900

    fix(ui): Spinner.svelteが中心を原点に回転するように

--- packages-js/ui/src/lib/Spinner.svelte
+++ packages-js/ui/src/lib/Spinner.svelte
@@ -1,25 +1,21 @@
-<script lang="ts">
-    export let active = true;
-</script>
-
-<i class="ti ti-loader-2" class:active />
-
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-loader-2">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 3a9 9 0 1 0 9 9" />
+</svg>
+  
 <style lang="scss">
-    i {
+    svg {
         display: inline-flex;
         align-items: center;
         justify-content: center;
         color: var(--color-1);
-        visibility: hidden;
-        background: transparent;
         border: none;
         animation: spin 1s linear infinite;
-        width: 1rem;
-        height: 1rem;
-
-        &.active {
-            visibility: visible;
-        }
+        transform-origin: center;
+        line-height: 1;
+        width: 1em;
+        height: 1em;
+        vertical-align: -0.125em;
     }
 
     @keyframes spin {
