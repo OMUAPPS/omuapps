79a1d12e53252e74fa2b62a55f1fe4b6a6f333b2
2.30am <111672334+am230@users.noreply.github.com>
2024-10-20 21:57:36 +0900

    fix(site): レジストリのデータを初期化される前に変更できないように

--- packages-js/site/src/lib/helper.ts
+++ packages-js/site/src/lib/helper.ts
@@ -2,8 +2,15 @@ import type { Registry } from '@omujs/omu/extension/registry/index.js';
 import { type Writable } from 'svelte/store';
 
 export function makeRegistryWritable<T>(registry: Registry<T>): Writable<T> {
+    let ready = false;
+    registry.listen(() => {
+        ready = true;
+    });
     return {
         set: (value: T) => {
+            if (!ready) {
+                throw new Error(`Registry ${registry.type.id.key()} is not ready`);
+            }
             registry.set(value);
         },
         subscribe: (run) => {
@@ -12,6 +19,9 @@ export function makeRegistryWritable<T>(registry: Registry<T>): Writable<T> {
             return unlisten;
         },
         update: (fn) => {
+            if (!ready) {
+                throw new Error('Registry is not ready');
+            }
             registry.update(fn);
         },
     };
