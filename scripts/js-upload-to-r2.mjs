4ff67b7394ceb31210455ce25fc306560f7acb51
2.30am <111672334+am230@users.noreply.github.com>
2025-01-21 17:10:28 +0900

    r2

--- scripts/js-upload-to-r2.mjs
+++ scripts/js-upload-to-r2.mjs
@@ -1,7 +1,7 @@
+import { context, getOctokit } from '@actions/github';
 import { execaSync } from 'execa';
 import { readdirSync } from 'fs';
 const option = { stderr: process.stderr, stdout: process.stdout }
-
 const options = (() => {
     const args = process.argv.slice(2);
     const options = {};
@@ -16,6 +16,25 @@ const options = (() => {
     return options;
 })();
 
+const github = getOctokit(process.env.GITHUB_TOKEN);
+const { owner, repo } = context.repo;
+const { data: releases } = await github.rest.repos.listReleases();
+console.log(releases);
+const release = releases.find(release => release.tag_name === options.tag);
+if (!release) {
+    throw new Error(`Release not found: ${options.tag}`);
+}
+const { data: assets } = await github.rest.repos.listReleaseAssets({
+    owner,
+    repo,
+    release_id: release.id,
+});
+// download assets to ./release-assets
+for (const asset of assets) {
+    const url = asset.browser_download_url;
+    const name = asset.name;
+    execaSync('curl', ['-LJO', url, '--header', 'User-Agent: pnpm', '--header', `Authorization: token ${process.env.GITHUB_TOKEN}`, '-o', `./release-assets/${name}`], option);
+}
 
 const BASE_URL = 'https://obj.omuapps.com/'
 const BUCKET = 'omuapps-app'
