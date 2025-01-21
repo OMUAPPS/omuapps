a934f5609b4a2e08d30eeb30357ade4286dbbe77
2.30am <111672334+am230@users.noreply.github.com>
2025-01-21 20:06:24 +0900

    Update js-upload-to-r2.mjs

--- scripts/js-upload-to-r2.mjs
+++ scripts/js-upload-to-r2.mjs
@@ -15,7 +15,8 @@ const options = (() => {
     }
     return options;
 })();
-
+const version = options.version;
+const tag = `app-v${version}`;
 const github = getOctokit(process.env.GITHUB_TOKEN);
 const { owner, repo } = context.repo;
 const { data: releases } = await github.rest.repos.listReleases({
@@ -23,9 +24,9 @@ const { data: releases } = await github.rest.repos.listReleases({
     repo,
 });
 console.log(releases);
-const release = releases.find(release => release.tag_name === options.tag);
+const release = releases.find(release => release.tag_name === tag);
 if (!release) {
-    throw new Error(`Release not found: ${options.tag}`);
+    throw new Error(`Release not found: ${tag}`);
 }
 const { data: assets } = await github.rest.repos.listReleaseAssets({
     owner,
@@ -42,11 +43,45 @@ for (const asset of assets) {
 
 const BASE_URL = 'https://obj.omuapps.com/'
 const BUCKET = 'omuapps-app'
-const PATH = 'app'
+const PATH = `app/${tag}`
 
 const files = await fs.readdir('./release-assets');
-console.log(files);
-const urls = files.map(async file => {
+const urls = Object.fromEntries(files.map(async file => {
     execaSync('bash', ['-c', `cat ./release-assets/${file} | pnpm wrangler r2 object put ${BUCKET}/${PATH}/${file} --pipe`], option);
-    return `${BASE_URL}/${PATH}/${file}`;
-});
+    return [file, `${BASE_URL}/${PATH}/${file}`];
+}));
+execaSync('bash', ['-c', `cat ./release-assets/latest-beta.json | pnpm wrangler r2 object put ${BUCKET}/app/latest-beta.json --pipe`], option);
+const platforms = [
+    {
+        type: 'windows-x86_64',
+        filename: '{NAME_UPPER}_{VERSION}_x64-setup.exe',
+    },
+    {
+        type: 'linux-x86_64',
+        filename: '{NAME_LOWER}_{VERSION}_amd64.AppImage.tar.gz',
+    },
+    {
+        type: 'darwin-aarch64',
+        filename: '{NAME_UPPER}_aarch64.app.tar.gz',
+    },
+    {
+        type: 'darwin-x86_64',
+        filename: '{NAME_UPPER}_x64.app.tar.gz',
+    },
+];
+const releaseData = {
+    version: options.version,
+    notes: release.body,
+    pub_date: new Date(release.published_at).toISOString(),
+    platforms: Object.fromEntries(platforms.map(async platform => {
+        const { type, filename } = platform;
+        const name = filename
+            .replace('{NAME_UPPER}', 'OMUAPPS')
+            .replace('{NAME_LOWER}', 'omuapps')
+            .replace('{VERSION}', options.version);
+        const url = urls[name];
+        return [type, { url }];
+    })),
+};
+await fs.writeFile('./release-assets/version-beta.json', JSON.stringify(releaseData, null, 4));
+execaSync('bash', ['-c', `cat ./release-assets/version-beta.json | pnpm wrangler r2 object put ${BUCKET}/app/version-beta.json --pipe`], option);
