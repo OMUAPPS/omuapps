2a13744d456fec884b9bc5a7e5bca8e26e3ad719
2.30am <111672334+am230@users.noreply.github.com>
2025-02-09 23:22:20 +0900

    chore(site): ドキュメント更新時に自動で再読込するようにする

--- packages-js/site/vite.config.ts
+++ packages-js/site/vite.config.ts
@@ -1,9 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { searchForWorkspaceRoot } from 'vite';
+import fs from 'fs/promises';
+import { searchForWorkspaceRoot, type PluginOption } from 'vite';
 import { defineConfig } from 'vitest/config';
 
+const reloadPlugin = (): PluginOption => ({
+    name: 'reload',
+    configureServer(server) {
+        const path = searchForWorkspaceRoot(process.cwd() + '..') + '/documentation';
+        (async () => {
+            const watcher = fs.watch(path, { recursive: true });
+            for await (const event of watcher) {
+                if (event.eventType !== 'change') continue;
+                server.watcher.emit('change', event.filename);
+                console.log('[doc]', event.filename);
+            }
+        })();
+        server.watcher.on('change', (file) => {
+            if (!file.endsWith('.md')) return;
+            server.ws.send({ type: 'full-reload' });
+        });
+    },
+});
+
 export default defineConfig({
-    plugins: [sveltekit()],
+    plugins: [sveltekit(), reloadPlugin()],
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}'],
     },
