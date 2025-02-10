0e07e09ea06da57a062142492629f4a4727c9fc2
2.30am <111672334+am230@users.noreply.github.com>
2025-02-11 01:51:08 +0900

    Update tauri.ts

--- packages-js/dash/src/lib/tauri.ts
+++ packages-js/dash/src/lib/tauri.ts
@@ -257,9 +257,6 @@ import { Menu } from '@tauri-apps/api/menu/menu';
 import { TrayIcon } from '@tauri-apps/api/tray';
 
 waitForTauri().then(async () => {
-    if (await TrayIcon.getById('OMUAPPS-tray')) {
-        return;
-    }
     let visible = false;
     visible = await appWindow.isVisible();
 
