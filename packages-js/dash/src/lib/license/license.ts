d08e8370304da1fcd0216a7cfca879a173145165
2.30am <111672334+am230@users.noreply.github.com>
2025-02-11 22:56:55 +0900

    fix(dash): licenses.jsonのインポート方法を更新

--- packages-js/dash/src/lib/license/license.ts
+++ packages-js/dash/src/lib/license/license.ts
@@ -6,7 +6,7 @@ interface License {
     licenseText?: string;
 }
 
-import licenses from './licenses.json';
+import licenses from './licenses.json' with { type: 'json' };
 import * as TablerIcons from './tabler-icons.js';
 
 export const LICENSES: License[] = [TablerIcons, ...(licenses as License[])];
