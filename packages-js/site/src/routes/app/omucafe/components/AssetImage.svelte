2699f54ed05371b3ee8f6ae462564607e57e9aee
2.30am <111672334+am230@users.noreply.github.com>
2025-05-24 19:55:31 +0900

    fix(app-omucafe): アセット画像の表示をキャッシュさせる

--- packages-js/site/src/routes/app/omucafe/components/AssetImage.svelte
+++ packages-js/site/src/routes/app/omucafe/components/AssetImage.svelte
@@ -1,36 +1,15 @@
 <script lang="ts">
-    import { Identifier } from '@omujs/omu';
-    import { onDestroy } from 'svelte';
-    import type { Asset } from '../game/asset.js';
-    import { getGame } from '../omucafe-app.js';
+    import { getAsset, type Asset } from '../game/asset.js';
 
     export let asset: Asset | undefined;
-
-    const { omu } = getGame();
 
     let src: string | null = null;
 
     async function update(asset: Asset | undefined) {
-        if (src) {
-            URL.revokeObjectURL(src);
-        }
-        if (!asset) {
-            src = null;
-            return;
-        }
-        if (asset.type === 'url') {
-            src = asset.url;
-        } else if (asset.type === 'asset') {
-            const { buffer } = await omu.assets.download(Identifier.fromKey(asset.id));
-            src = URL.createObjectURL(new Blob([buffer]));
+        if (asset) {
+            src = (await getAsset(asset)).toString()
         }
     }
-
-    onDestroy(() => {
-        if (src) {
-            URL.revokeObjectURL(src);
-        }
-    });
 
     $: update(asset);
 </script>
