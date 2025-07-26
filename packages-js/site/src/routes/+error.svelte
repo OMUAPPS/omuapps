efa751d2323ead6b105bcec08c9ddd684638aaef
2.30am <111672334+am230@users.noreply.github.com>
2025-07-26 17:46:03 +0900

    feat(site): エラーページを追加

---
+++ packages-js/site/src/routes/+error.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    const error = ({
+        404: {
+            title: 'Not Found',
+            detail: 'ページが見つかりませんでした',
+        },
+        500: {
+            title: 'Internal Server Error',
+            detail: 'サーバーで問題が発生しました',
+        },
+    })[$page.status] || {
+        title: 'Error',
+        detail: '予期しないエラーが発生しました',
+    };
+</script>
+
+<main>
+    <h1>
+        {error.title}
+    </h1>
+    <small>
+        {error.detail}
+    </small>
+    <a href="/">
+        最初に戻る
+        <i class="ti ti-external-link"></i>
+    </a>
+</main>
+
+
+<style lang="scss">
+    main {
+        position: absolute;
+        inset: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding-bottom: 12%;
+    }
+
+    h1 {
+        color: var(--color-1);
+        border-bottom: 1px solid var(--color-1);
+        margin-bottom: 0.5rem;
+    }
+
+    small {
+        margin-bottom: 1rem;
+    }
+</style>
