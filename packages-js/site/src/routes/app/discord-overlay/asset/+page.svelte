e04eb6ddcbff9cf93b7cbd46ecafe5ed215b11ba
2.30am <111672334+am230@users.noreply.github.com>
2025-10-29 13:52:27 +0900

    fix(app-discord-overlay): アセット追加できるように

--- packages-js/site/src/routes/app/discord-overlay/asset/+page.svelte
+++ packages-js/site/src/routes/app/discord-overlay/asset/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-    import { page } from '$app/stores';
     import AssetPage from '$lib/components/AssetPage.svelte';
-    import { App, Omu } from '@omujs/omu';
-    import { setClient } from '@omujs/ui';
-    import { BROWSER } from 'esm-env';
-    import { APP_ID, ASSET_APP } from '../app.js';
+    import { ASSET_APP } from '../app.js';
     import AssetApp from './AssetApp.svelte';
-
-    let assetId = BROWSER && $page.url.searchParams.get('assetId');
-    const id = assetId || Date.now().toString();
-    const omu = new Omu(
-        new App(APP_ID.join('asset', id), {
-            version: '0.1.0',
-        }),
-    );
-    setClient(omu);
 </script>
 
 <AssetPage asset={ASSET_APP} let:omu>
