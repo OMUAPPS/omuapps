56640b151dcc63c4d578e36ebbfda5cb1cbae939
2.30am <111672334+am230@users.noreply.github.com>
2025-10-29 20:31:52 +0900

    fix(dash): アップデートの詳細表示を修正

--- packages-js/dash/src/routes/_components/StepUpdateHint.svelte
+++ packages-js/dash/src/routes/_components/StepUpdateHint.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
+    import Document from '$lib/common/Document.svelte';
     import type { Update } from '@tauri-apps/plugin-updater';
-    import SvelteMarkdown from 'svelte-markdown';
 
     export let update: Update;
 </script>
 
 <div class="hint">
-    <SvelteMarkdown source={update.body} />
+    <Document source={update.body ?? ''} />
 </div>
 
 <style lang="scss">
@@ -17,29 +17,5 @@
         padding-top: max(8rem, 12%);
         overflow-y: auto;
         height: 100%;
-
-        > :global(h1) {
-            margin-bottom: 1rem;
-        }
-
-        > :global(h2) {
-            margin-top: 1.5rem;
-            margin-bottom: 1rem;
-        }
-
-        > :global(h3) {
-            margin-top: 1rem;
-            margin-bottom: 0.5rem;
-        }
-
-        > :global(ul) {
-            margin-top: 1rem;
-            margin-bottom: 1rem;
-        }
-    }
-
-    h2 {
-        border-bottom: 1px solid var(--color-1);
-        padding-bottom: 0.5rem;
     }
 </style>
