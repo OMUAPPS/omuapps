a52db58c6794f454a2c5af7e4ab51cd6167f8a92
2.30am <111672334+am230@users.noreply.github.com>
2025-11-08 08:52:17 +0900

    chore: 古いのアプリを削除

--- packages-js/site/src/routes/app/apps.ts
+++ packages-js/site/src/routes/app/apps.ts
@@ -2,13 +2,11 @@ import { App } from '@omujs/omu';
 import { BROWSER, DEV } from 'esm-env';
 import { omu } from '../client.js';
 import { APP as fries } from './aoikuru-fries/app.js';
-import { APP as archive } from './archive/app.js';
 import { APP as breaktimer } from './break-timer/app.js';
 import { APP as caption } from './caption/app.js';
 import { APP as chatSubtitle } from './chatsubtitle/app.js';
 import { APP as clock } from './clock/app.js';
 import { APP as discordOverlay } from './discord-overlay/app.js';
-import { APP as emoji } from './emoji/app.js';
 import { APP as lipsynctest } from './lipsynctest/app.js';
 import { APP as marshmallow } from './marshmallow/app.js';
 import { APP as omucafe } from './omucafe/app.js';
@@ -18,7 +16,6 @@ import { APP as replay } from './replay/app.js';
 import { APP as roulette } from './roulette/app.js';
 import { APP as tester } from './tester/app.js';
 import { APP as timer } from './timer/app.js';
-import { APP as translator } from './translator/app.js';
 
 export const apps = [
     discordOverlay,
@@ -33,13 +30,10 @@ export const apps = [
 if (DEV) {
     apps.unshift(
         caption,
-        emoji,
         lipsynctest,
         tester,
-        translator,
         chatSubtitle,
         breaktimer,
-        archive,
         clock,
         remote,
     );
