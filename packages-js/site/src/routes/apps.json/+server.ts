03c822b508a24c67a607ce28ca6e30193c243e16
2.30am <111672334+am230@users.noreply.github.com>
2025-11-12 01:07:39 +0900

    fix(site): apps.jsonのidがベータで切り替わるように修正

--- packages-js/site/src/routes/apps.json/+server.ts
+++ packages-js/site/src/routes/apps.json/+server.ts
@@ -1,13 +1,14 @@
-import { App } from '@omujs/omu';
+import { App, Identifier } from '@omujs/omu';
 import { json } from '@sveltejs/kit';
 import { apps } from '../app/apps';
+import { NAMESPACE } from '../app/origin';
 import type { RequestHandler } from './$types';
 
 export const prerender = true;
 
 export const GET: RequestHandler = () => {
     return json({
-        id: 'com.omuapps:apps',
+        id: new Identifier(NAMESPACE, 'apps'),
         apps: Object.fromEntries(apps.map((app) => [app.id.key(), App.serialize(app)])),
     });
 };
