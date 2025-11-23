78604821ea906937ea6cb7033a04d15eb92d25fe
2.30am <111672334+am230@users.noreply.github.com>
2025-11-24 08:40:09 +0900

    Update js-publish.ts

--- scripts/js-publish.ts
+++ scripts/js-publish.ts
@@ -6,16 +6,10 @@ async function publishPackage(target: string) {
     await $`bun publish --cwd ${target} --access public`;
 }
 
-await Promise.all([
-    publishPackage('packages-js/i18n'),
-    publishPackage('packages-js/omu'),
-]);
-await Promise.all([
-    publishPackage('packages-js/chat'),
-    publishPackage('packages-js/plugin-obs'),
-]);
+await publishPackage('packages-js/i18n');
+await publishPackage('packages-js/omu');
+await publishPackage('packages-js/chat');
+await publishPackage('packages-js/plugin-obs');
 
 await fs.rm(Path.join(process.cwd(), 'dist'), { recursive: true, force: true });
-await Promise.all([
-    publishPackage('packages-js/ui'),
-]);
+await publishPackage('packages-js/ui');
