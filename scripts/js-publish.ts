541c71b37be49d669d7a5df130640b11bfa3eaea
2.30am <111672334+am230@users.noreply.github.com>
2025-12-18 13:05:14 +0900

    ci: npmへの公開にnpmを使うようにする

--- scripts/js-publish.ts
+++ scripts/js-publish.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 import Path from 'path';
 
 async function publishPackage(target: string) {
-    await $`bun publish --cwd ${target} --access public --tolerate-republish`;
+    await $`npm publish --access public --tolerate-republish`.cwd(target).nothrow();
 }
 
 await publishPackage('packages-js/i18n');
